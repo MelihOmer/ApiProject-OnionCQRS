eadf8a27a202b5e1eb8375730e4e83845f164ccc
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-01-21 18:38:41 +0300

    Temel entityler oluşturuldu.

---
+++ Core/ApiProject.Domain/Common/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProject.Domain.Common
+{
+    public class EntityBase : IEntityBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
