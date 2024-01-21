6bc39819fd3323eacc46c2a5ae4cba5e98e85f2e
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-01-21 19:59:38 +0300

    AppDbContext oluşturuldu. EntityConfig sınıfları yazıldı. Seed datalar hazırlandı. Migration oluşturuldu. Dbinit edildi.

---
+++ Infrastructure/ApiProject.Persistence/Configurations/CategoryConfig.cs
@@ -0,0 +1,46 @@
+﻿using ApiProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApiProject.Persistence.Configurations
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            List<Category> categories = new()
+            {
+                new Category
+                {
+                    Id=1,
+                    Name = "Elektronik",
+                    Priority=1,
+                    ParentId=0
+                },
+                new Category
+                {
+                    Id=2,
+                    Name = "Moda",
+                    Priority=2,
+                    ParentId=0
+                },
+                new Category
+                {
+                    Id=3,
+                    Name = "Bilgisayar",
+                    Priority=1,
+                    ParentId=1
+                },
+                new Category
+                {
+                    Id=4,
+                    Name = "Kadın",
+                    Priority=1,
+                    ParentId=2,
+                }
+            };
+            builder.HasData(categories);
+        }
+    }
+
+}
---
+++ Infrastructure/ApiProject.Persistence/Configurations/ProductConfig.cs
@@ -0,0 +1,37 @@
+﻿using ApiProject.Domain.Entities;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApiProject.Persistence.Configurations
+{
+    public class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            Faker faker = new("tr");
+            List<Product> products = new()
+            {
+                new Product
+                {
+                    Id = 1,
+                    Title = faker.Commerce.ProductName(),
+                    Description = faker.Commerce.ProductDescription(),
+                    BrandId =1,
+                    Discount=faker.Random.Decimal(0,10),
+                    Price = faker.Finance.Amount(10,1000)
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = faker.Commerce.ProductName(),
+                    Description = faker.Commerce.ProductDescription(),
+                    BrandId =3,
+                    Discount=faker.Random.Decimal(0,10),
+                    Price = faker.Finance.Amount(10,1000)
+                }
+            };
+            builder.HasData(products);
+        }
+    }
+}
