40b20e3fce1a11c1a02ed89b4d65f34cabcbcfb9
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-01-25 20:27:02 +0300

    Mediatr Paket Kuruldu. Product/getAll CQRS pattern test edildi.

---
+++ Core/ApiProject.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ApiProject.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryRequest: IRequest<IList<GetAllProductsQueryResponse>>
+    {
+        
+    }
+}
