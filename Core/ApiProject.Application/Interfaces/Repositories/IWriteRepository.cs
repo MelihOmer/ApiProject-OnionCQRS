c7036a903da059bf2c77160bce7ce23f93da506d
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-01-25 19:06:57 +0300

    WriteRepository Yazıldı.

--- Core/ApiProject.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/ApiProject.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace ApiProject.Application.Interfaces.Repositories
+﻿using ApiProject.Domain.Common;
+
+namespace ApiProject.Application.Interfaces.Repositories
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T> where T : class,IEntityBase,new()
     {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IList<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task HardDeleteAsync(T entity);
+        Task SoftDeleteAsync(T entity);
+
     }
 }
