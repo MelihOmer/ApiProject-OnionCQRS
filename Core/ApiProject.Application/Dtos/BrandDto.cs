c6d574fa9ed872a00dbc0516da1a6006055a3f56
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-01-27 18:43:46 +0300

    AutoMapper dahil edildi custom Mapper yazıldı ve test olarak kullanıldı.

---
+++ Core/ApiProject.Application/Dtos/BrandDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiProject.Application.Dtos
+{
+    public class BrandDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Core/ApiProject.Mapper/Registration.cs
@@ -0,0 +1,13 @@
+﻿using ApiProject.Application.Interfaces.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiProject.Mapper
+{
+    public static class Registration
+    {
+        public static void AddMapper(this IServiceCollection services)
+        {
+            services.AddSingleton<IMapper, AutoMapper.Mapper>();
+        }
+    }
+}
