cf4ec399d7fc3fedc953df5a71210bab11842436
Melih Ömer Kamar <melih.o.k@hotmail.com>
2024-02-03 00:33:02 +0300

    RedisCacheService, setting ve PipelineBehevior yazıldı. settings yapılandırıldı.

---
+++ Infrastructure/ApiProject.Infrastructure/RedisCache/RedisCacheService.cs
@@ -0,0 +1,34 @@
+﻿using ApiProject.Application.Interfaces.RedisCache;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace ApiProject.Infrastructure.RedisCache
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly ConnectionMultiplexer redisConnection;
+        private readonly IDatabase database;
+        private readonly RedisCacheSettings redisCacheSettings;
+        public RedisCacheService(IOptions<RedisCacheSettings> option)
+        {
+            redisCacheSettings = option.Value;
+            var opt = ConfigurationOptions.Parse(configuration: redisCacheSettings.ConnectionString);
+            redisConnection = ConnectionMultiplexer.Connect(opt);
+            database = redisConnection.GetDatabase();
+        }
+        public async Task<T> GetAsync<T>(string key)
+        {
+            var value = await database.StringGetAsync(key);
+            if (value.HasValue)
+                return JsonConvert.DeserializeObject<T>(value);
+            return default;
+        }
+
+        public async Task SetAsync<T>(string key, T value, DateTime? expirationTime = null)
+        {
+            TimeSpan? timeExpiration = expirationTime.Value - DateTime.Now;
+            await database.StringSetAsync(key, JsonConvert.SerializeObject(value),timeExpiration);
+        }
+    }
+}
